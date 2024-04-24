7bcbc88b0122c72d2337ee60fb958a6fc16e5723
Camilo Carmona Ramirez <camilocr3@hotmail.com>
2024-04-23 20:52:13 -0500

    Componetizando

---
+++ src/app/dbz/components/List/list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-list-dbz',
+    templateUrl: 'list.component.html'
+})
+
+export class ListDbzComponent{
+    constructor() { }
+}
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainDbzComponent } from './pages/main-page.component';
+import { ListDbzComponent } from './components/List/list.component';
 
 
 
 @NgModule({
   declarations: [
-    MainDbzComponent
+    MainDbzComponent,
+    ListDbzComponent
   ],
   imports: [
     CommonModule
